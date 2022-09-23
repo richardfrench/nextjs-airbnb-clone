9648cf769ad2078650b0bcdc81c73cb0b6440030
Richard French <richard.french@gmail.com>
2022-09-23 12:49:37 +0100

    Add scraped data

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,24 +1,12 @@
 import { Heading, Text, Box } from "@cruk/cruk-react-components";
-import { NasaSearchParams } from "../../types";
-import Results from "../Results";
-
+import properties from '../../ldn-limited.json';
 export const HomePage = () => {
-  //
-  // form and validation code here
-
-  const exampleParam: NasaSearchParams = {
-    keywords: "moon",
-    yearStart: 2000,
-    mediaType: "image",
-  };
 
   return (
     <Box marginTop="s" paddingTop="s">
       <Heading h1>React Exercise</Heading>
-
-      <Text>Form goes here</Text>
-
-      <Results searchParams={exampleParam} />
+      {properties.map((item) => (
+      ))}
     </Box>
   );
 };
